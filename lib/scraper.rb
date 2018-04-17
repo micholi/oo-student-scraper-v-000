e4f07927c8b8816c2558ff6967a3ff6896ccecbf
Michelle Olivieri <michelleo72@gmail.com>
2018-04-17 15:03:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,8 +22,6 @@ class Scraper
     end
 
   def self.scrape_profile_page(profile_url)
-    profile_url
-    binding.pry
     profile_page = Nokogiri::HTML(open(profile_url))
     student_profile = {}
     social_links = profile_page.css(".social-icon-container").children.css("a").map { |z| z.attribute('href').value}
